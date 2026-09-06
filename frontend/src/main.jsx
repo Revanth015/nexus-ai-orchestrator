d9754bdf5458831ad6dea797286362a86d216665
Revanth015 <115963624+Revanth015@users.noreply.github.com>
2026-09-06 21:41:36 +0530

    feat: make workspace the primary NEXUS interface

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import ManagerDashboard from "./ManagerDashboard";
+import NexusWorkspace from "./NexusWorkspace";
 import "./styles.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ManagerDashboard />
+    <NexusWorkspace />
   </React.StrictMode>
 );
